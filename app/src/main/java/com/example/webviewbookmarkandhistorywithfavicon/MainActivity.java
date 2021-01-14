278c30e4b585bc24e51e08361dc7dc53bee568c3
abelfn <70645835+abelfn@users.noreply.github.com>
2021-01-14 07:33:27 -0800

    Update MainActivity.java

--- app/src/main/java/com/example/webviewbookmarkandhistorywithfavicon/MainActivity.java
+++ app/src/main/java/com/example/webviewbookmarkandhistorywithfavicon/MainActivity.java
@@ -107,13 +107,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 if(dbHelperb.checkif(BookMarkInsertSetterGetter.getUrl())){
-                    Toast.makeText(MenuActivity.this, "BookMark exists", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "BookMark exists", Toast.LENGTH_SHORT).show();
                 }else{
+                    dbHelperb.open();
                     dbHelperb.insertBookMarks(new BookMarkHistoryItems(BookMarkInsertSetterGetter.getFavicon(), BookMarkInsertSetterGetter.getTitle(), BookMarkInsertSetterGetter.getUrl()));
 
                     UpdateBookMarkFavicon task = new UpdateBookMarkFavicon(BookMarkUpdateSetterGetter.getUrl(),BookMarkUpdateSetterGetter.getOriginalUrl(), BookMarkUpdateSetterGetter.getFavicon());
                     task.execute();
-                    Toast.makeText(MenuActivity.this, "Bookmark Saved", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Bookmark Saved", Toast.LENGTH_SHORT).show();
                 }
 
             }
